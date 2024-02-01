db6a23c097b5779ba64c68f3ddd542d5cdbaa135
Towsif20 <aaatowsif16@gmail.com>
2024-02-01 13:40:57 +0600

    Adjusted exception handler

--- src/main/java/com/towsif/PlayerManagementSystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/towsif/PlayerManagementSystem/exception/GlobalExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.towsif.PlayerManagementSystem.exception;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ValidationException;
 import org.springframework.data.mapping.PropertyReferenceException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,34 +10,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler
 {
-    @ExceptionHandler(EntityNotFoundException.class)
-    public Object handleEntityNotFoundException(EntityNotFoundException exception, HttpServletRequest request, Model model)
+    @ExceptionHandler({
+            EntityNotFoundException.class,
+            PropertyReferenceException.class,
+            ValidationException.class,
+            ClassCastException.class
+    })
+    public Object handleBadRequests(RuntimeException exception, Model model)
     {
-        boolean isApiRequest = isApiRequest(request);
-
-        if(isApiRequest)
-            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-
         model.addAttribute("errorMsg", exception.getMessage());
 
         return "error";
-    }
-
-    @ExceptionHandler({PropertyReferenceException.class, ValidationException.class, ClassCastException.class})
-    public Object handleBadRequests(RuntimeException exception, HttpServletRequest request, Model model)
-    {
-        boolean isApiRequest = isApiRequest(request);
-
-        if(isApiRequest)
-            return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
-
-        model.addAttribute("errorMsg", exception.getMessage());
-
-        return "error";
-    }
-
-    private static boolean isApiRequest(HttpServletRequest request)
-    {
-        return request.getRequestURI().startsWith("/api");
     }
 }
